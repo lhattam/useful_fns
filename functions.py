c1863d6a4e36ed32fc7e1b42e7e58f082f3a09bd
Laura Hattam <ljh86@bath.ac.uk>
2019-09-24 14:40:15 +0000

    typo

--- functions.py
+++ functions.py
@@ -8,7 +8,7 @@ def mean(list):
     total=0
     list_ex=[1.0,2.0]
     if type(list)!=type(list_ex)
-        return "You didn't pASS A LIST"
+        return "You didn't pass a LIST"
     for item in list:
         total+=item
     return total/len(list)
